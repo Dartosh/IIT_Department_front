51373d010d12791f45d1e8930081d76806caccb0
Антон Клинцов <92170048@study.bsuir.by>
2021-05-26 02:59:46 +0300

    feat: update pop up login

---
+++ scripts/pop-up-menu.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  //Скрыть PopUp при загрузке страницы    
+  //PopUpHide();
+});
+//Функция отображения PopUp
+function PopUpShow(){
+  $("#log-popup").show();
+}
+//Функция скрытия PopUp
+function PopUpHide(){
+  $("#log-popup").hide();
+}
